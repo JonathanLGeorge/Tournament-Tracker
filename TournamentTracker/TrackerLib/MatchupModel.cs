b93097a240bd635395ea41ac2871e074470aaa29
JonathanLGeorge <jgeorge12@horizon.csueastbay.edu>
2019-08-10 01:08:13 -0700

    bed time

--- TournamentTracker/TrackerLib/MatchupModel.cs
+++ TournamentTracker/TrackerLib/MatchupModel.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace TrackerLib
 {
+    /// <summary>
+    /// This is one match in tournament
+    /// </summary>
     public class MatchupModel
     {
+
+      /// <summary>
+      /// This list of teams that competed in this match
+      /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// <summary>
+        /// the round this match was apart of 
+        /// </summary>
         public int MatchupRound { get; set; }
+
+        /// <summary>
+        /// the winner of the match
+        /// </summary>
         public TeamModel Winner { get; set; }
     }
 }
--- TournamentTracker/TrackerLib/PersonModel.cs
+++ TournamentTracker/TrackerLib/PersonModel.cs
@@ -8,9 +8,24 @@ namespace TrackerLib
 {
     public class PersonModel
     {
+        /// <summary>
+        /// First name of the member
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// last name of the member
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// email of the member
+        /// </summary>
         public string EmailAdress { get; set; }
+
+        /// <summary>
+        /// cellphone number of the member
+        /// </summary>
         public string CellNumber { get; set; }
 
     }
